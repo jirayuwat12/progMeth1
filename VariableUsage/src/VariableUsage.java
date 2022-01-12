96b359c5af7e7abcc61be76c6cf440a2f2ce5da6
jirayuwat12 <49447426+jirayuwat12@users.noreply.github.com>
2022-01-12 15:16:28 +0700

    variable usage

---
+++ VariableUsage/src/VariableUsage.java
@@ -0,0 +1,19 @@
+
+public class VariableUsage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=1;
+		final int a_const = 2;
+		
+		double b=1.0f;
+		final double b_const = 2.0f;
+		
+		String c = "jirayuwat";
+		final String c_const = "jirayuwat";
+		
+		System.out.print(a+"\n"+b + "\n"+ c);
+		System.out.println(a_const + "\n" + b_const + "\n"+ c_const);
+	}
+
+}
